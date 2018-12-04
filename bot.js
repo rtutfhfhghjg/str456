1be2b16a25921c52681ac817f796552d3561219b
rtutfhfhghjg <45015125+rtutfhfhghjg@users.noreply.github.com>
2018-12-04 22:06:05 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+console.log('By : m7md');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'iiZeRvX_',
+     details: `Hi`,
+     url: 'http://twitch.tv/M7md_Salih',
+     state: `هاي يوتيوب`,
+    application_id: '377479790195769345',
+     assets: {
+        small_image: `519595977972252673`,
+        small_text: 'هاي يوتيوب',
+        large_image: `519595977972252673`,
+        large_text: `iiZeRvX_ forever`
+` }
+
+  }
+    });
+});
+اقولها
+
+client.login("NDYwMTI1MDAzNzc4ODgzNTg1.Duhn8Q.RqeB0UPDRxNtXR27fxeLj4LJoQU");
